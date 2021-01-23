61db6b7d3d6c31b6c3151fb0dab2099f3141c358
aya-elsherbiny <ayaahmedelsherbiny@gmail.com>
2021-01-23 21:53:32 +0200

    adding Components folder by aya

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './Components/menu/menu.component';
+import { OffersComponent } from './Components/offers/offers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    OffersComponent
   ],
   imports: [
     BrowserModule,
