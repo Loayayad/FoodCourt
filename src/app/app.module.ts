997832c967915804bc55a54941db0a05ead6b210
Noha Sabri <nohasbr@gmail.com>
2021-01-23 11:55:49 -0800

    adding chefs commponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChefsComponent } from './Components/chefs/chefs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChefsComponent
   ],
   imports: [
     BrowserModule,
