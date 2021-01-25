a3a626f32bfef304698e30f442ea305ad9a2cf2a
loay zakaria <lo2lo2.loay@gmail.com>
2021-01-25 22:58:18 +0200

    fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { ChefsComponent } from './Components/chefs/chefs.component';
-=======
+
 import { MenuComponent } from './Components/menu/menu.component';
 import { OffersComponent } from './Components/offers/offers.component';
-<<<<<<< HEAD
 import { CartComponent } from './Components/cart/cart.component';
-=======
->>>>>>> 61db6b7d3d6c31b6c3151fb0dab2099f3141c358
->>>>>>> d845eb7a3c59a27d4fcf38c3805c167a332317bf
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ChefsComponent
-=======
+    ChefsComponent,
     MenuComponent,
-<<<<<<< HEAD
     OffersComponent,
-    CartComponent
-=======
+    CartComponent,
     OffersComponent
->>>>>>> 61db6b7d3d6c31b6c3151fb0dab2099f3141c358
->>>>>>> d845eb7a3c59a27d4fcf38c3805c167a332317bf
   ],
   imports: [
     BrowserModule,
