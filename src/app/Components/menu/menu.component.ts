324c836e89e77152c1d47212d94c44d8c3ee8f73
aya-elsherbiny <ayaahmedelsherbiny@gmail.com>
2021-01-26 22:49:59 +0200

    menu displaying all meals for now

--- src/app/Components/menu/menu.component.ts
+++ src/app/Components/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MealAPIService } from 'src/app/Services/meal-api.service';
+import { IMeal } from 'src/app/ViewModels/imeal';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  subscription: Subscription|null = null;
+  MealsList: IMeal[] = [];
+  constructor(private mealService:MealAPIService) { }
 
   ngOnInit(): void {
+    this.subscription = this.mealService.getAllMeals().subscribe(
+      (res)=>{
+        this.MealsList = res;
+      },
+      (err)=>{
+        console.log(err);
+      }
+    )
+    console.log(this.MealsList);
   }
-
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
