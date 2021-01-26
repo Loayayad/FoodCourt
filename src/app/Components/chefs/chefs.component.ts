6cb5f0f4411502db2bc1d1eb30a64a7dceb422a8
Noha Sabri <nohasbr@gmail.com>
2021-01-26 13:43:35 -0800

    Chefs component

--- src/app/Components/chefs/chefs.component.ts
+++ src/app/Components/chefs/chefs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router}from '@angular/router';
+import {ChefService} from 'src/app/Services/chef.service';
+import {IChefs} from 'src/app/ViewModels/ichefs';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-chefs',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chefs.component.scss']
 })
 export class ChefsComponent implements OnInit {
-
-  constructor() { }
-
+   ChefList:IChefs[]=[];
+   subscription: Subscription|null=null;
+  constructor(
+    private router:Router,
+    private chefServiceApi:ChefService,
+  ) { }
+  
   ngOnInit(): void {
-  }
+    this.subscription= this.chefServiceApi.getAllChefs().subscribe(
+      (response)=>{
+        this.ChefList = response;
+      },
+      (err)=>{console.log(err)}
+    );
 
+  }
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
--- src/app/Services/chef.service.ts
+++ src/app/Services/chef.service.ts
@@ -1,9 +1,21 @@
+import{ HttpClient,HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
+import {IChefs} from '../ViewModels/ichefs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChefService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getAllChefs():Observable<IChefs[]>{
+
+    return this.http.get<IChefs[]>(`${environment.API_URL}/chefs`);
+  }
+  getChefsByID(pID:number):Observable<IChefs>
+  {
+    return this.http.get<IChefs>(`${environment.API_URL}/chefs/${pID}`);
+  }
 }
