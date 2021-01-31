e9189abb86e2faab489420bc0fe4e13ab1415570
NourhanSalah <norahansalah1997@gmail.com>
2021-01-31 21:18:29 +0200

    editing cart

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -8,8 +8,11 @@ import { MenuComponent } from '../menu/menu.component';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-
-  
+  totalPrice:number[]=[];
+  // subTotal:number=this.totalPrice.reduce()
+    subTotal:number=0;
+    taxes:number=0.14;
+    total:number=0;
   
   items = this.cartService.getItems();
   
@@ -20,5 +23,23 @@ export class CartComponent implements OnInit {
   ngAfterViewInit(){
     
   }
+  ngOnChanges(){
+    
+  }
+  addMeal(count:string,price:number){
+    
 
+    
+    this.totalPrice.push(parseInt(count)*price);
+  }
+   
+    
+  
+  calcTotalPrice(){
+    for(var i=0; i<this.totalPrice.length;i++)
+    {
+      this.subTotal+=this.totalPrice[i];
+    }
+    this.total = (this.taxes*this.subTotal)+this.subTotal+20;
+  }
 }
