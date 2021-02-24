fb76602092e815b2111a1ea8a7dfd2a0b104e8e9
aya-elsherbiny <ayaahmedelsherbiny@gmail.com>
2021-02-25 00:02:33 +0200

    adding orders to the firebase firestore

--- src/app/Services/cart.service.ts
+++ src/app/Services/cart.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { BehaviorSubject } from 'rxjs';
+import { isConstructorDeclaration } from 'typescript';
 import { IMeal } from '../ViewModels/imeal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  id:number=1;
   items: {mealImage:string,mealID:number, mealName:string, mealPrice:number, mealShow:boolean, mealDiscount:string, mealCount:number}[]=[];
   orders: {purchasedMeals:{mealName:string, mealCount:number,mealPrice:number, mealDiscount:string, totalPrice:number}[]
            ,userID:string|null}[]=[];
   purchasedItems: {mealName:string, mealCount:number,mealPrice:number, mealDiscount:string, totalPrice:number}[]=[];
   totalPrice:number=0;
+  constructor(private afs: AngularFirestore){}
   addTocart(mealImage:string,mealID:number, mealName:string, mealPrice:number, mealShow:boolean, mealDiscount:string, mealCount:number){
     // this.items.forEach((element)=>{
     //     if(element.mealID == mealID){
@@ -21,6 +25,7 @@ export class CartService {
     //     return ;
     // }) 
     this.items.push({mealImage,mealID,mealName,mealPrice, mealShow, mealDiscount, mealCount});
+  
   }
 
   moveToCheckOut(mealName:string, mealCount:number, mealPrice:number, mealDiscount:string, totalPrice:number){
@@ -46,6 +51,21 @@ export class CartService {
   postOrders(purchasedMeals:{mealName:string, mealCount:number,mealPrice:number, mealDiscount:string, totalPrice:number}[],
              userID:string|null){
     this.orders.push({purchasedMeals, userID});
+    for(var i=0; i<this.orders.length; i++){
+      for(var j=0; j<this.orders[i].purchasedMeals.length; j++)
+      {
+        this.afs.collection("orders").doc(`${this.orders[i].userID}`).set({
+          id: this.orders[i].userID,
+          name: this.orders[i].purchasedMeals[j].mealName,
+          count: Number(this.orders[i].purchasedMeals[j].mealCount),
+          price: Number(this.orders[i].purchasedMeals[j].mealPrice),
+          total: Number(this.orders[i].purchasedMeals[j].totalPrice),
+          discount: this.orders[i].purchasedMeals[j].mealDiscount,
+          userID: this.orders[i].userID
+        })
+      }
+      
+    }
   }
 
   getOrders(){
